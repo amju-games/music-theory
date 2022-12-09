9352fecafb393b429149062ec2392c50c4304005
Juliet Colman <juliet@amjula.com>
2022-12-09 15:03:45 +0000

    Tidy up Question.h

--- Source/Question.h
+++ Source/Question.h
@@ -8,66 +8,32 @@
 #include <ConfigFile.h>
 #include <RCPtr.h>
 #include <StringUtils.h>
-#include "MusicalTermsDictionary.h"
 
 namespace Amju
 {
-  // There could be multiple ways to phrase the same question. E.g.
-  //  What is the meaning of this Italian musical term
-  //  What does this term mean? 
-  //  Choose the meaning for this musical term:
-  // So maybe just choose at random from a list of strings.
-  // Different Q types: what is English for this (Italian etc) term,
-  //  what is (Italian) term which means (English)
-  // This is different data, not different behaviour.
+// Base class for questions, the logical, non-presentation representation of questions
+//  we ask the user. So should be completely testable... right?
 
-  class Question : public RefCounted
-  {
-  public:
-    // Get a question string at random
-    // TODO what about testing
-    virtual std::string GetQuestionString();
+class Question : public RefCounted
+{
+public:
+  virtual ~Question() = default;
 
-    //virtual bool Load(File*); // load question strings
+  // Get a question string at random
+  // TODO what about testing
+  virtual std::string GetQuestionString();
 
-    void AddQuestionString(const std::string& qs);
+  void AddQuestionString(const std::string& qs);
 
-    virtual void MakeQuestion() = 0;
+  virtual void MakeQuestion() = 0;
 
-    // Get/set flag in config file, so we know if a question has been seen before.
-    // (We give more info if not seen before, etc.)
-    virtual bool QuestionSeenBefore(ConfigFile*) const = 0;
-    virtual void SetQuestionSeenBefore(ConfigFile*) const = 0;
+  // Get/set flag in config file, so we know if a question has been seen before.
+  // (We give more info if not seen before, etc.)
+  virtual bool QuestionSeenBefore(ConfigFile*) const = 0;
+  virtual void SetQuestionSeenBefore(ConfigFile*) const = 0;
 
-  protected:
-    Strings m_questionStrings; // typically, choose one of these at random - could be default impl of GetQString
-  };
-
-  // TODO split into files
-
-
-
-  //class Music;
-
-  //// Ask user to pick interval from a list, 
-  //// OR PLAY IT? No, but the HINT could show notes on a keyboard.
-  //class IntervalQuestion : public MultiChoiceQuestion
-  //{
-  //  // qString will be { "What is this interval?", "Name the interval".. etc
-
-  //  // We need a type which represents musical score/notation
-  //  const Music& GetIntervalAsMusic(); // to display interval as a score. 
-
-  //};
-
-  //class PlayKeyboardQuestion : public Question
-  //{
-
-  //};
-
-  //class PlayTheNotesQuestion : public PlayKeyboardQuestion
-  //{
-  //};
-
+protected:
+  Strings m_questionStrings; // typically, choose one of these at random - could be default impl of GetQString
+};
 }
 
