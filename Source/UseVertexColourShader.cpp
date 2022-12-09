5e7132887c7d925fa147533ce6e97f7ad819f1d9
Juliet Colman <juliet@amjula.com>
2022-12-09 15:04:18 +0000

    Factor out function into its own file. This func is only required for desktop OpenGL. Shouldn't be a member of GSBase.

---
+++ Source/UseVertexColourShader.cpp
@@ -0,0 +1,22 @@
+// * Amjula music theory *
+// (c) Copyright 2017 Jason Colman
+
+#include <AmjuGL.h>
+#include "UseVertexColourShader.h"
+
+namespace Amju
+{
+
+void UseVertexColourShader()
+{
+#if defined(WIN32) || defined(MACOSX)
+  // Set shader for desktop GL - fixed function doesn't seem to treat vertex colours the
+  //  way we want (i.e. multiply by currently active colour)
+  static Shader* sh = AmjuGL::LoadShader("Shaders/" + AmjuGL::GetShaderDir() + "/gui");
+  Assert(sh);
+  AmjuGL::UseShader(sh);
+#endif
+}
+
+}
+
---
+++ Source/UseVertexColourShader.h
@@ -0,0 +1,12 @@
+// * Amjula music theory *
+// (c) Copyright 2017 Jason Colman
+
+#pragma once
+
+namespace Amju
+{
+// Use shader which correctly combines global and vertex colours - 
+//  apparently necessary for desktop OGL?
+void UseVertexColourShader();
+}
+
