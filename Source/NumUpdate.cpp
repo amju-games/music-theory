ed3d6334232de942aebdaec667cf5b6eeb98fb08
Juliet Colman <juliet@amjula.com>
2022-12-09 15:04:19 +0000

    No more drop shadow

--- Source/NumUpdate.cpp
+++ Source/NumUpdate.cpp
@@ -18,18 +18,16 @@ void NumUpdate(PGuiElement gui, const std::string& elemName, const std::string& 
   // Find element
   IGuiText* text = dynamic_cast<IGuiText*>(gui->GetElementByName(elemName));
   Assert(text);
-  text->SetText(value);
 
-  //text = dynamic_cast<IGuiText*>(gui->GetElementByName(elemName + "-shadow"));
-  //if (text)
-  //{
-  //  text->SetText(ToString(value));
-  //}
-
-  GuiDecAnimation* anim = dynamic_cast<GuiDecAnimation*>(gui->GetElementByName(elemName + "-anim-trigger"));
-  if (anim)
+  if (text->GetText() != value)
   {
-    anim->ResetAnimation();
+    text->SetText(value);
+
+    GuiDecAnimation* anim = dynamic_cast<GuiDecAnimation*>(gui->GetElementByName(elemName + "-anim-trigger"));
+    if (anim)
+    {
+      anim->ResetAnimation();
+    }
   }
 }
 }
