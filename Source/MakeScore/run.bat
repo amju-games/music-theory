bf061dab5999a58db86ceeb17e71766731cd84a2
Juliet Colman <juliet@amjula.com>
2022-12-09 15:04:31 +0000

    MakeScore: Show final rendered output from MakeScore on Windows

---
+++ Source/MakeScore/run.bat
@@ -0,0 +1,14 @@
+REM @echo off
+REM Run MusicTheory app to display output from MakeScore
+
+REM Run MakeScore, write output to out.txt
+echo "4/4 <c> 60 62 64 65 | 67 69 71 72" | ..\..\Build\MakeScore\Debug\MakeScore.exe --stave-single --oneline > out.txt
+
+REM Append output to boilerplate GUI file
+copy ..\..\Assets\Gui\scoretest.txt ..\..\Assets\Gui\gs_test_show_score.txt
+type out.txt >>  ..\..\Assets\Gui\gs_test_show_score.txt
+
+REM Run MusicTheory app - build with GSTestShowScore as the active state
+cd ..\..\Assets
+start ..\Build\Debug\AmjulaMusicTheory.exe
+cd ..\Source\MakeScore
