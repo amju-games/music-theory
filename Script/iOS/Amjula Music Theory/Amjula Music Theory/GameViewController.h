5a76c6b09f589eff2db262fcb349af4c55dd8ffa
Juliet Colman <juliet@amjula.com>
2022-12-09 15:03:35 +0000

    Add Xcode/iOS project: currently unmodified OpenGL ES sample

---
+++ Script/iOS/Amjula Music Theory/Amjula Music Theory/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  Amjula Music Theory
+//
+//  Created by JC on 18/03/2017.
+//  Copyright © 2017 Amju. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <GLKit/GLKit.h>
+
+@interface GameViewController : GLKViewController
+
+@end
