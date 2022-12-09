a1f6c9c888b19fa3d01c0e935e9837d6df23d0d7
Juliet Colman <juliet@amjula.com>
2022-12-09 15:04:28 +0000

    Add empty GSAvatarMod class

---
+++ Source/GSAvatarMod.cpp
@@ -0,0 +1,9 @@
+// * Amjula music theory *
+// (c) Copyright 2017-2018 Jason Colman
+
+#include "GSAvatarMod.h"
+
+namespace Amju
+{
+}
+
---
+++ Source/GSAvatarMod.h
@@ -0,0 +1,16 @@
+// * Amjula music theory *
+// (c) Copyright 2017-2018 Jason Colman
+
+#pragma once
+
+#include "GSBase.h"
+
+namespace Amju
+{
+class GSAvatarMod : public GSBase
+{
+public:
+  
+};
+}
+
