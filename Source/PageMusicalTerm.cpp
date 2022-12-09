218b7396e7f6d05b2aa757191cb022bb60699b90
Juliet Colman <juliet@amjula.com>
2022-12-09 15:03:45 +0000

    Tidying

--- Source/PageMusicalTerm.cpp
+++ Source/PageMusicalTerm.cpp
@@ -1,13 +1,9 @@
 // * Amjula music theory *
 // (c) Copyright 2017 Jason Colman
 
-#include <ConfigFile.h>
-#include <GuiButton.h>
-#include <GuiDecAnimation.h>
+#include <GuiText.h>
 #include <ResourceManager.h>
-#include "GSPages.h"
 #include "PageMusicalTerm.h"
-#include "PrintGui.h"
 
 namespace Amju
 {
@@ -15,34 +11,30 @@ const char* PageMusicalTerm::NAME = "page-musical-term";
 
 void PageMusicalTerm::OnActive()
 {
-  m_question = new MusicalTermQuestion;
+  MusicalTermQuestion* q = new MusicalTermQuestion;
+  m_question = q; 
 
   // Get musical terms dictionary (TODO select grade)
   // This is a Resource.
   MusicalTermsDictionary* dic = dynamic_cast<MusicalTermsDictionary*>(
     TheResourceManager::Instance()->GetRes("Course/musical_terms.txt.dictionary"));
 
-  MusicalTermQuestion* q = dynamic_cast<MusicalTermQuestion*>(m_question.GetPtr());
   q->SetDictionary(dic);
   q->MakeQuestion();
   m_answers = q->GetMultiChoiceAnswers();
 
   // Seen this question before? If not we will give the correct answer.
-  bool seen = m_question->QuestionSeenBefore(m_config);
+  bool seen = q->QuestionSeenBefore(m_config);
   // Set flag for next time
-  m_question->SetQuestionSeenBefore(m_config);
+  q->SetQuestionSeenBefore(m_config);
 
   m_canRemoveForHint.clear();
 
+  // Load gui, set button callbacks
   PageMultiChoice::OnActive();
 
   IGuiText* text = dynamic_cast<IGuiText*>(GetElementByName(m_gui, "musical-term-text"));
   text->SetText(q->GetMusicalTermText());
-}
-
-void PageMusicalTerm::SetMusicalTermQuestion(MusicalTermQuestion* q)
-{
-  m_question = q;
 }
 
 }
