a33f63c9aaa673cbf6603c8e5c0376986ab6d7f7
Juliet Colman <juliet@amjula.com>
2022-12-09 15:03:39 +0000

    Add gtest framework

---
+++ Source/Tests/QuestionTests.cpp
@@ -0,0 +1,14 @@
+// -----------------------------------------------------------------------------
+// glboiler - Jason Colman 2016 - OpenGL experiments
+// -----------------------------------------------------------------------------
+
+#include <gtest/gtest.h>
+#include "Question.h"
+
+using namespace Amju;
+
+TEST(MusicalTermQuestion, default_ctor)
+{
+  MusicalTermQuestion q;
+}
+
