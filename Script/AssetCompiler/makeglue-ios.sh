09fb20fbebad34b5627741869b0482671a303484
Juliet Colman <juliet@amjula.com>
2022-12-09 15:04:00 +0000

    Scripts to make glue files for iOS (also work for Mac and Win)

--- Script/AssetCompiler/makeglue-ios.sh
+++ Script/AssetCompiler/makeglue-ios.sh
@@ -17,6 +17,8 @@ mkdir $DEST_DIR/Gui
 mkdir $DEST_DIR/Image
 mkdir $DEST_DIR/Scene
 mkdir -p $DEST_DIR/Shaders/gles
+# Not required for iOS but lets us use the same glue file for iOS, Mac and Win
+mkdir -p $DEST_DIR/Shaders/opengl
 mkdir -p $DEST_DIR/Sound/wav
 
 cp $SRC_DIR/*.txt $DEST_DIR
@@ -24,18 +26,28 @@ cp $SRC_DIR/Course/*.txt $DEST_DIR/Course/
 cp -R $SRC_DIR/font2d/* $DEST_DIR/font2d
 cp $SRC_DIR/Gui/*.txt $DEST_DIR/Gui
 
+# TODO platform-specific GUI files
 #cp $SRC_DIR/gui/$PLATFORM/*.txt $DEST_DIR
-cp $SRC_DIR/Image/*.png $DEST_DIR/Image
-#cp $SRC_DIR/tex/$PLATFORM/*.png $DEST_DIR
-cp $SRC_DIR/Scene/*.txt $DEST_DIR/Scene
 
+cp $SRC_DIR/Image/*.png $DEST_DIR/Image
+cp $SRC_DIR/Shaders/gles/*.txt $DEST_DIR/Shaders/gles/
+# Not required for iOS but lets us use the same glue file for iOS, Mac and Win
+cp $SRC_DIR/Shaders/opengl/*.txt $DEST_DIR/Shaders/opengl/
+cp $SRC_DIR/Scene/*.txt $DEST_DIR/Scene
+# Images in obj/ are copied to the top level dest dir. We get the full path by using the 
+#  path to the obj file, but in a glue file this is ignored
+cp $SRC_DIR/obj/*.png $DEST_DIR
+# mtl files and images used to decorate meshes - 
+#  TODO put in separate dir so we only add minimal set
+cp $SRC_DIR/obj/*.mtl $DEST_DIR/obj
+cp $SRC_DIR/obj/*.png $DEST_DIR/obj
 
 cd $DEST_DIR
 
 # Create empty glue file
 $GLUE_EXE -c $GLUE_FILE
 
-for f in *.txt Course/*.txt Image/*.png obj/*.obj 
+for f in *.txt *.png Course/*.txt Image/*.png obj/*.obj obj/*.mtl obj/*.png Gui/*.txt Scene/*.txt font2d/*.txt font2d/ArialRound/* font2d/Dimbo/* font2d/Guido2/* Shaders/gles/*.txt Shaders/opengl/*.txt
 do
     echo "Adding file: " $f
     $GLUE_EXE -a $GLUE_FILE $f
--- Script/AssetCompiler/makemusicglue-ios.sh
+++ Script/AssetCompiler/makemusicglue-ios.sh
@@ -10,17 +10,18 @@ export GLUE_EXE=$THIS_DIR/glue
 export GLUE_FILE=$DEST_DIR/../music-$PLATFORM.glue
 
 mkdir $COMPILED_ASSETS
-mkdir -p $DEST_DIR/sound
+mkdir -p $DEST_DIR/Music
+mkdir -p $DEST_DIR/Sound/wav
 
-cp $SRC_DIR/sound/*.it $DEST_DIR/sound
-cp $SRC_DIR/sound/windows/* $DEST_DIR/sound
+cp $SRC_DIR/Music/*.it $DEST_DIR/Music
+cp $SRC_DIR/Sound/wav/*.wav $DEST_DIR/Sound/wav
 
 cd $DEST_DIR
 
 # Create empty glue file
 $GLUE_EXE -c $GLUE_FILE
 
-for f in sound/*.wav sound/*.it
+for f in Sound/wav/*.wav Music/*.it
 do
   echo "Adding file: " $f
   $GLUE_EXE -a $GLUE_FILE $f
