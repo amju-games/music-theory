9d734966c2104bfd842c6b5ecd53701e681f8c45
Juliet Colman <juliet@amjula.com>
2022-12-09 15:03:39 +0000

    Panel type - used to display Questions? WIP

---
+++ Source/Panel.h
@@ -0,0 +1,41 @@
+// * Amjula music theory *
+// (c) Copyright 2017 Jason Colman
+
+#pragma once
+
+// Experimental types for defining a Question, Answer and Hint on a page.
+
+namespace Amju
+{
+class Panel
+{
+public:
+  virtual ~Panel() {}
+  virtual void OnActive() {};
+  virtual void OnDeactive() {};
+
+};
+
+class AnswerPanel : public Panel
+{
+};
+
+class QuestionPanel : public Panel
+{
+};
+
+// One page has a series of hints, which are unlocked one at a time. The final hint gives the answer.
+// Also for new question types, we should show the answer.
+// Hints could affect the appearance of the Question and the Answer!
+// E.g.: to show interval, we may want to highlight a note on the stave (Question) while showing
+//  the same note on a keyboard in the Hint area.
+// For MCQ, we might want to pulse the correct answer..?
+// So we can't split up the Page into Question/Answer/Hint..?
+// Just keep protoyping, design is not clear to me yet.
+class HintPanel : public Panel
+{
+};
+
+
+}
+
