ff07a9659ab846302bff4e486c94769e9331ecfd
Juliet Colman <juliet@amjula.com>
2022-12-09 15:03:46 +0000

    Add musical symbol page

---
+++ Source/PageMusicalSymbol.cpp
@@ -0,0 +1,16 @@
+// * Amjula music theory *
+// (c) Copyright 2017 Jason Colman
+
+#include "PageMusicalSymbol.h"
+
+namespace Amju
+{
+const char* PageMusicalSymbol::NAME = "page-musical-symbol";
+
+PageMusicalSymbol::PageMusicalSymbol()
+{
+  m_guiName = "musical_symbol";
+  m_dictionaryFilename = "Course/musical_symbols.txt.dictionary";
+}
+
+}
