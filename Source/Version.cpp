8351121a306f474adba211a8cfde64cfde9512e7
Juliet Colman <juliet@amjula.com>
2024-04-30 20:21:26 +0100

    Update version format to major.minor.patch

--- Source/Version.cpp
+++ Source/Version.cpp
@@ -3,14 +3,17 @@
 
 #include "Version.h"
 
-#define AMJU_VERSION_MAJOR 1
-#define AMJU_VERSION_MINOR 0
+#define AMJU_VERSION_MAJOR 0
+#define AMJU_VERSION_MINOR 1
+#define AMJU_VERSION_PATCH 0
 
 namespace Amju
 {
 std::string GetVersionStr()
 {
-  return std::to_string(AMJU_VERSION_MAJOR) + "." + std::to_string(AMJU_VERSION_MINOR);
+  return std::to_string(AMJU_VERSION_MAJOR) + "." + 
+    std::to_string(AMJU_VERSION_MINOR) + "." +
+    std::to_string(AMJU_VERSION_PATCH);
 }
 }
 
