b29dae8558a65e66c32e946bfc824f53bc9c12b2
Juliet <juliet.e.colman@gmail.com>
2025-12-22 19:45:52 +0000

    Not really a rename -- replace script that does one job

--- Script/Macosx/setup_make_vars.sh
+++ Script/Macosx/setup_make_vars.sh
@@ -1,8 +1,6 @@
-# Run Amjula Music Theory for MacOSX
+# Set up env vars for make -- SOURCE ME with . ./setup_make_vars.sh
 export AMJU_BUILD_ARCH=Mac
 export AMJU_MACHDEP='-DMACOSX -DXP_MACOSX -DAMJU_USE_BASS -DAMJU_USE_CURL -DUSE_SHADOW_MAP_OPENGL_2'
 
 export AMJULIB_MAC_SCRIPT_PATH=`pwd`/../../../amjulib/Script/MacOSX/
-
-make runnoglue
 
