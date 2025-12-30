09d17827756c687350ad09d6f5b289d97b1faef7
Juliet <juliet.e.colman@gmail.com>
2025-12-30 13:23:49 +0000

    Remove tests for deleted features

--- Source/MakeScore/Tests/MakeScore_catchtest.cpp
+++ Source/MakeScore/Tests/MakeScore_catchtest.cpp
@@ -20,30 +20,4 @@ TEST_CASE("Test MakeScore Broken", "MakeScore")
   REQUIRE(ms.ToString() == "crotchet");
 }
 
-TEST_CASE("Load sample music xml file", "MakeScore")
-{
-  // test1 has 
-  //  - one part
-  //  - one stave
-  //  - one bar
-  //    -  treble clef, key of c major, 4/4, one semibreve middle C.
-
-  MakeScore ms;
-  bool successfulLoad = ms.LoadXml("test1.musicxml");
-  REQUIRE(successfulLoad);
-  REQUIRE(ms.NumParts() == 1);
-  REQUIRE(ms.NumBars() == 1);
-}
-
-
-TEST_CASE("Compare outputs from MusicXML and shorthand inputs", "MakeScore")
-{
-  MakeScore xmlScore;
-  REQUIRE(xmlScore.LoadXml("test1.musicxml"));
-
-  MakeScore shorthandScore("4/4 clef-t key-s-0 <sb> 60");
-  shorthandScore.MakeInternal();
-
-  REQUIRE(xmlScore.ToString() == shorthandScore.ToString());
-}
 
