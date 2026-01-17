e7b90fba5645025d30d93ed1bd8591bb9626fb84
Juliet <juliet.e.colman@gmail.com>
2026-01-17 19:42:04 +0000

    MakeScore: test/example scripts

--- Source/MakeScore/Examples/tied-notes.sh
+++ Source/MakeScore/Examples/tied-notes.sh
@@ -1,12 +1,15 @@
 # * Amjula Music Theory *
 # (c) Copyright 2026 Juliet Colman
 
-../makescore "page-w 4 <g4> c t c t m | <c> a4 t a4 t <m> a4" > out.txt
+
+../makescore "page-w 4 [Tied notes] <d4> c t c | <g5> c t c " > out.txt
+
+cat out.txt
 
 # Append output to boilerplate
+# TODO We can load score from out.txt now
 cp ../../../Assets/Gui/scoretest.txt ../../../Assets/Gui/scoretest_temp.txt
 cat out.txt >> ../../../Assets/Gui/scoretest_temp.txt
-cat out.txt
 # Run the game in 'view score' mode to display the score.
 ../../../Build/Mac/macgame --viewscore Gui/scoretest_temp.txt
 
