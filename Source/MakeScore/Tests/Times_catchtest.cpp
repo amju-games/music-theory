ee285d6498370c620d538a371f0265d9b79ad420
Juliet <juliet.e.colman@gmail.com>
2026-02-26 10:15:36 +0000

    MakeScore: tests for time types

--- Source/MakeScore/Tests/Times_catchtest.cpp
+++ Source/MakeScore/Tests/Times_catchtest.cpp
@@ -4,6 +4,10 @@
 
 TEST_CASE("Set Times from token", "[Times]")
 {
+  REQUIRE(Times("qqq").GetTimeType() == TimeType::QQQ);
+  REQUIRE(Times("qqq.").GetTimeType() == TimeType::DOTTED_QQQ);
+  REQUIRE(Times("qq").GetTimeType() == TimeType::SEMIQUAVER);
+  REQUIRE(Times("qq.").GetTimeType() == TimeType::DOTTED_SEMIQUAVER);
   REQUIRE(Times("c").GetTimeType() == TimeType::CROTCHET);
   REQUIRE(Times("c.").GetTimeType() == TimeType::DOTTED_CROTCHET);
   REQUIRE(Times("m").GetTimeType() == TimeType::MINIM);
