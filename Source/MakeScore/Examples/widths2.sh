8e8cfbfc864bd51d169e8a4d80bc6f1cbfe98ab0
Juliet <juliet.e.colman@gmail.com>
2026-03-11 18:19:39 +0000

    MakeScore: examples for bar width adjustment

---
+++ Source/MakeScore/Examples/widths2.sh
@@ -0,0 +1,13 @@
+# * Amjula Music Theory *
+# (c) Copyright 2026 Juliet Colman
+
+# Test width scaling: 2 staves
+../makescore --scale 0.5 "page-w 4 4/4 <sb> r | <qq> g3 a3 b3 c4 d4 e4 f4+ g4 a4 b4 c5 d5 e5 f5+ <q> g5 | t sb | stave 4/4 <qq> g3 a3 b3 c4 d4 e4 f4+ g4 a4 b4 c5 d5 e5 f5+ g5 a5 | t <sb> g5 | r |" > out.txt
+
+# Append output to boilerplate
+cp ../../../Assets/Gui/scoretest.txt ../../../Assets/Gui/scoretest_temp.txt
+cat out.txt >> ../../../Assets/Gui/scoretest_temp.txt
+cat out.txt
+# Run the game in 'view score' mode to display the score.
+../../../Build/Mac/macgame --viewscore Gui/scoretest_temp.txt
+
