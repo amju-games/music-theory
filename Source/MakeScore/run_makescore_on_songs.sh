136f20cd5e14958711ef109a56bfda5287772c8a
Juliet <juliet.e.colman@gmail.com>
2026-03-28 11:13:38 +0000

    Script to run MakeScore on all songs. Best to comment out all songs and uncomment selectively, as this file grows.

---
+++ Source/MakeScore/run_makescore_on_songs.sh
@@ -0,0 +1,6 @@
+# Run makescore to generate scores for songs.
+# Are you sure you want to? It could be introducing more bugs on tested songs.
+# Only run this on every song if there's a mega improvement in makescore!
+
+./makescore --file ../../Assets/Songs/Satie-Gymnopedie-1/satie-gymnopedie-1.makescore.txt > ../../Assets/Songs/Satie-Gymnopedie-1/satie-gymnopedie-1.score.txt
+
