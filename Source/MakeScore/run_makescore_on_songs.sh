f9a060130b34a70da8636dcc89d21e186417fcf1
Juliet <juliet.e.colman@gmail.com>
2026-03-28 11:52:46 +0000

    Run makescore on all three songs, to get bar numbers and improve ties.

--- Source/MakeScore/run_makescore_on_songs.sh
+++ Source/MakeScore/run_makescore_on_songs.sh
@@ -4,3 +4,6 @@
 
 ./makescore --file ../../Assets/Songs/Satie-Gymnopedie-1/satie-gymnopedie-1.makescore.txt > ../../Assets/Songs/Satie-Gymnopedie-1/satie-gymnopedie-1.score.txt
 
+./makescore --file ../../Assets/Songs/Bach-Air/bach-air.makescore.txt > ../../Assets/Songs/Bach-Air/bach-air.score.txt
+
+./makescore --file ../../Assets/Songs/SaintSaens-TheSwan/saintsaens-theswan.makescore.txt > ../../Assets/Songs/SaintSaens-TheSwan/saintsaens-theswan.score.txt 
