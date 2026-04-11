4e3af697bb6f205d88da60899373d76a1b045b72
Juliet <juliet.e.colman@gmail.com>
2026-04-11 21:28:35 +0100

    PushShader/PopShader: at long last. TODO promote to AmjuGL.

--- Source/UseVertexColourShader.cpp
+++ Source/UseVertexColourShader.cpp
@@ -2,17 +2,34 @@
 // (c) Copyright 2024 Juliet Colman
 
 #include <AmjuGL.h>
+#include <Shader.h>
 #include "UseVertexColourShader.h"
 
 namespace Amju
 {
+static std::vector<PShader> s_shaderVec;
+
+// TODO promote to AmjuGL
+void PushShader()
+{
+  s_shaderVec.push_back(AmjuGL::GetCurrentShader());
+}
+
+// TODO promote to AmjuGL
+void PopShader()
+{
+  auto shader = s_shaderVec.back();
+  s_shaderVec.pop_back();
+  AmjuGL::UseShader(shader);
+}
 
 void UseVertexColourShader()
 {
 #if defined(WIN32) || defined(MACOSX)
   // Set shader for desktop GL - fixed function doesn't seem to treat vertex colours the
   //  way we want (i.e. multiply by currently active colour)
-  static Shader* sh = AmjuGL::LoadShader("Shaders/" + AmjuGL::GetShaderDir() + "/gui");
+  static PShader sh = AmjuGL::LoadShader(
+    "Shaders/" + AmjuGL::GetShaderDir() + "/gui");
   Assert(sh);
   AmjuGL::UseShader(sh);
 #endif
--- Source/UseVertexColourShader.h
+++ Source/UseVertexColourShader.h
@@ -5,6 +5,12 @@
 
 namespace Amju
 {
+// TODO promote to AmjuGL
+void PushShader();
+
+// TODO promote to AmjuGL
+void PopShader();
+
 // Use shader which correctly combines global and vertex colours - 
 //  apparently necessary for desktop OGL?
 void UseVertexColourShader();
