19f54b64de995f5c72c327cc7d914e1bfca675c5
Juliet <juliet@amju.com>
2026-04-30 14:31:17 +0100

    MAKEGLUE: small tidy up. Unfortunately text files in the Songs/Tools dir are being added to the glue file; TODO prevent that.

--- Script/AssetCompiler/makeglue-ios.sh
+++ Script/AssetCompiler/makeglue-ios.sh
@@ -11,11 +11,11 @@ export GLUE_EXE="$THIS_DIR/glue"
 export GLUE_FILE="$DEST_DIR/../data-$PLATFORM.glue"
 
 # 2. Create all directories at once (-p creates parents and ignores existing)
-mkdir -p "$DEST_DIR"/{Course,font2d,Gui/Palettes,Image,md2,obj,Scene,Shaders/gles,Shaders/opengl,Sound/wav,Songs}
+mkdir -p "$DEST_DIR"/{font2d,Image,md2,obj,Scene,Shaders/gles,Shaders/opengl,Sound/wav,Songs}
 
 # 3. Copy Assets (Simplified with wildcards)
-cp "$SRC_DIR"/*.{txt,csv} "$DEST_DIR/" 2>/dev/null
-cp -R "$SRC_DIR"/{Course,font2d,Gui,Songs,md2} "$DEST_DIR/"
+cp "$SRC_DIR"/*.txt "$DEST_DIR/" 
+cp -R "$SRC_DIR"/{font2d,Gui,Songs,md2} "$DEST_DIR/"
 cp "$SRC_DIR"/Image/*.png "$DEST_DIR/Image/"
 cp "$SRC_DIR"/Shaders/gles/*.txt "$DEST_DIR/Shaders/gles/"
 cp "$SRC_DIR"/Shaders/opengl/*.txt "$DEST_DIR/Shaders/opengl/"
@@ -33,7 +33,7 @@ cd "$DEST_DIR"
 
 # 6. Add all files to Glue
 # Instead of listing 50 paths, we find all files we care about:
-find . -type f \( -name "*.txt" -o -name "*.csv" -o -name "*.png" -o -name "*.obj" -o -name "*.mtl" -o -path "./md2/*" \) | while read -r f; do
+find . -type f \( -name "*.txt" -o -name "*.csv" -o -name "*.png" -o -name "*.obj" -o -name "*.mtl" -o -path "./md2/*.md2" \) | while read -r f; do
     # Remove the './' from the start of the filename for the glue tool
     clean_f="${f#./}"
     echo "Adding file: $clean_f"
