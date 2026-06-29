c1753b760bf12833e7d7eb2b1b7809735109b12b
Juliet <juliet@amju.com>
2026-06-29 16:25:52 +0100

    Makescore: improved example, shows correct spacing for a chord with multiple accidentals.

--- Source/MakeScore/Examples/layout-grid-5.sh
+++ Source/MakeScore/Examples/layout-grid-5.sh
@@ -2,7 +2,7 @@
 # (c) Copyright 2026 Juliet Colman
 
 # Test width scaling
-../makescore --scale 0.5 "[Layout Grid 5] page-w 4.75 4/4 <sb> c5 | <qq> 48 49 50 51 52 53 54 55 56 57 58 59 60 61 62 63 |" > out.txt
+../makescore --scale 0.5 "[Layout Grid 5] page-w 4.75 4/4 <sb> c5 | <qq> 48 49 50 51 52 53 54 55 56 57 ( 58 61 e4- ) 59 60 61 62 63 |" > out.txt
 
 # Append output to boilerplate
 cp ../../../Assets/Gui/scoretest.txt ../../../Assets/Gui/scoretest_temp.txt
