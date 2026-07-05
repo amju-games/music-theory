23eb7c93cbbbc919ffbaed2d598b97f7c98c15d9
Juliet <juliet@amju.com>
2026-07-05 10:56:36 +0100

    Feedback balloon: set background size to text. Reduce GUI error checking to Asserts, because the GUI is set at build time. Remove annoying sound effect.

--- Source/FeedbackBalloon.cpp
+++ Source/FeedbackBalloon.cpp
@@ -8,47 +8,42 @@ namespace Amju
 void SetFeedbackBalloonVisible(bool showNotHide, PGuiElement gui)
 {
   auto elem = GetElementByName(gui, "feedback-balloon");
-  if (elem)
-  {
-    elem->SetVisible(showNotHide);
-  }
+  Assert(elem);
+  elem->SetVisible(showNotHide);
 }
 
 void SetUpFeedbackBalloon(const Grade& g, PGuiElement gui)
 {
   Assert(g.m_type != Grade::UNGRADED);
 
-  if (g.m_score > 0.5f)
-  {
-    // Sounds terrible //PlayWav("good1");
-  }
-  else 
-  {
-    //PlayWav("rubber_ducky");
-  }
-
   SetFeedbackBalloonVisible(true, gui);
 
+  // Set text inside balloon
+  Vec2f textSize;
   auto elem = GetElementByName(gui, "feedback-text");
-  if (elem)
-  {
-    auto t = dynamic_cast<IGuiText*>(elem);
-    if (t)
-    {    
-      t->SetText(g.m_feedback);
-    }    
-  }
+  Assert(elem); // feedback balloon gui error?!
+  auto t = dynamic_cast<GuiTextBase*>(elem);
+  Assert(t);
+  t->SetText(g.m_feedback);
+  textSize = t->CalcSizeToText();
+
+  // Set size of background images to fit text, plus a margin.
+  const float MARGIN = 0.1f;
+  elem = GetElementByName(gui, "bg1"); // TODO name
+  Assert(elem);
+  Vec2f bgSize = elem->GetSize();
+  bgSize.x = textSize.x + MARGIN;
+  elem->SetSize(bgSize);
+  elem = GetElementByName(gui, "bg2"); // TODO name
+  Assert(elem);
+  elem->SetSize(bgSize); // same size initially but grow at different rates.
 
   // Reset animation
   elem = GetElementByName(gui, "moving-anim");
-  if (elem)
-  {
-    auto a = dynamic_cast<GuiDecAnimation*>(elem);
-    if (a)
-    {
-      a->ResetAnimation();
-    }
-  }
+  Assert(elem);
+  auto a = dynamic_cast<GuiDecAnimation*>(elem);
+  Assert(a);
+  a->ResetAnimation();
 
   // TODO Set balloon colours according to score
 }
