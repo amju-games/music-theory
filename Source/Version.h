c01d874a2fc5a0fd2c02f58582528f1542405e9c
Juliet <juliet@amju.com>
2026-07-05 17:52:22 +0100

    Version.h: don't gitignore it. Even though it gets overwritten by the iOS release script, we can use it on master to show the version. The release script will only be running on an RC branch, no?

---
+++ Source/Version.h
@@ -0,0 +1,23 @@
+// AUTO-GENERATED
+// * Amjula music theory *
+// (c) Copyright 2026 Juliet Colman
+
+#pragma once
+
+#include <tuple>
+
+namespace Amju
+{
+static const char* const VERSION_STRING = "0.3.0";
+static const int MAJOR = 0;
+static const int MINOR = 3;
+static const int PATCH = 0;
+static const int BUILD = 0;
+
+// Version comparison helper
+inline bool IsVersionAtLeast(int major, int minor, int patch) 
+{
+  return std::tie(MAJOR, MINOR, PATCH) >= std::tie(major, minor, patch);
+}
+}
+
