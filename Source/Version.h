3dcdf62a118c36782360bf8d5cf9b38d4312e20f
Juliet <juliet@amju.com>
2026-07-06 13:14:48 +0100

    XCode build fixes: * DO * cherry pick to master

--- Source/Version.h
+++ Source/Version.h
@@ -8,11 +8,11 @@
 
 namespace Amju
 {
-static const char* const VERSION_STRING = "0.3.0";
+static const char* const VERSION_STRING = "0.3.0 (1)";
 static const int MAJOR = 0;
 static const int MINOR = 3;
 static const int PATCH = 0;
-static const int BUILD = 0;
+static const int BUILD = 1;
 
 // Version comparison helper
 inline bool IsVersionAtLeast(int major, int minor, int patch) 
