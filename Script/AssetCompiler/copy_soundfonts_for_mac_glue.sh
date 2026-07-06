3585a82a22d8038dbf5692e182fb2f5350a2a9dd
Juliet <juliet@amju.com>
2026-07-06 11:42:41 +0100

    Copy sound fonts for Mac + glue test build -- soundfonts are not added to glue files, because we can't load them from glue files using the BASS MIDI API. * DO * cherry pick this to master.

---
+++ Script/AssetCompiler/copy_soundfonts_for_mac_glue.sh
@@ -0,0 +1,6 @@
+# Copy Sound/ dir with soundfonts, for mac build with glue files
+#  enabled -- sound fonts are not part of the glue files.
+
+mkdir ../../Build/CompiledAssets/Sound
+cp ../../Assets/Sound/*.sf2 ../../Build/CompiledAssets/Sound
+
