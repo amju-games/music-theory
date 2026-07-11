268096098b84625442f38303e33d493c5f9086b8
Juliet <juliet@amju.com>
2026-07-11 12:26:24 +0100

    GSAnimals: render a basic scene, using persp or ortho camera. Both work, which is good -- we want ortho in game, persp in GSTitle. But we want the animals to be populated in code, not in a scene file.

--- Source/GSAnimals.cpp
+++ Source/GSAnimals.cpp
@@ -1,9 +1,24 @@
+#include <CommandLineArgs.h>
 #include "GSAnimals.h"
 
 namespace Amju
 {
 GSAnimals::GSAnimals()
 {
+  const auto& args = GetCommandLineArgs();
+  
+  // Get the final command line arg string and use it as scene filename
+  const auto& strings = args.GetArgs();
+  if (strings.size() > 2)
+  {   
+    m_sceneFilename = strings.back();
+  }   
+  else
+  {
+    // Fallback scene file
+    m_sceneFilename = "Scene/animals-persp.txt";
+  }
+
   m_guiFilename = "Gui/no_gui.txt";
 }
 }
