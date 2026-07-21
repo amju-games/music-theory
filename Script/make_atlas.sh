59a2bfe67dba6887620ed4d8cc1bea8c7cd1f0d0
Juliet <juliet@amju.com>
2026-07-21 14:14:08 +0100

    Image atlas: all extras images should live on one big atlas. Added script and tool to build atlas from individual images. Updated some gui files to use the atlas but Buttons don't use sprites... :(

---
+++ Script/make_atlas.sh
@@ -0,0 +1,21 @@
+# Create texture atlas
+
+IMG=../Assets/Image
+DEST=$IMG/atlas.png
+ATLAS=../Tools/atlas
+
+# Create atlas, 1024*1024, with a 16*16 grid
+$ATLAS create $DEST 1024 1024 16 16 --overwrite
+
+# Add heart, cell 0 in 4*4 grid
+$ATLAS replace $IMG/heart.png $DEST 4 4 0 --overwrite
+
+# Add circle, cell 1 in 4*4 grid
+$ATLAS replace $IMG/circle.png $DEST 4 4 1 --overwrite
+
+# Rough oval: under heart. Cell 8 in 4*8 grid
+$ATLAS replace $IMG/rough-oval.png $DEST 4 8 8 --overwrite
+
+# Rough circle: under circle. Cell 18 in 8 * 8 grid
+$ATLAS replace $IMG/rough-circle.png $DEST 8 8 18 --overwrite
+
