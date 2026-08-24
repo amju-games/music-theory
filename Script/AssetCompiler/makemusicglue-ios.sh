fc0dbc1c5c4df13bd77e3a8d824a7322e1140461
Juliet <juliet@amju.com>
2026-08-24 20:02:12 +0100

    iOS/Mac makemusicglue script: add soundfonts.

--- Script/AssetCompiler/makemusicglue-ios.sh
+++ Script/AssetCompiler/makemusicglue-ios.sh
@@ -12,10 +12,14 @@ export GLUE_EXE=$THIS_DIR/glue
 export GLUE_FILE=$DEST_DIR/../music-$PLATFORM.glue
 
 mkdir $COMPILED_ASSETS
-# No need for Music - use Songs for game state music.
+
+# Music - use for incidental music, TODO
+
 mkdir -p $DEST_DIR/Sound/wav
 rm $DEST_DIR/Sound/wav/*.wav
+rm -f $DEST_DIR/Sound/*.sf2
 cp $SRC_DIR/Sound/wav/*.wav $DEST_DIR/Sound/wav
+cp $SRC_DIR/Sound/*.sf2 $DEST_DIR/Sound
 
 rm $DEST_DIR/Songs/**/*.mid
 cp -R $SRC_DIR/Songs $DEST_DIR
@@ -24,6 +28,12 @@ cd $DEST_DIR
 
 # Create empty glue file
 $GLUE_EXE -c $GLUE_FILE
+
+for f in Sound/*.sf2 
+do
+  echo "Adding file: " $f
+  $GLUE_EXE -a $GLUE_FILE $f
+done
 
 for f in Sound/wav/*.wav 
 do
