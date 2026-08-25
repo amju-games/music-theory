66456df29f8c382b226cf86d7af498aa4f462975
Juliet <juliet@amju.com>
2026-08-25 20:09:25 +0100

    Windows: add application icon

---
+++ Assets/Windows/make_icon.bat
@@ -0,0 +1,9 @@
+REM PIANO FEST (c) Copyright 2026 Juliet Colman
+REM make icon for windows app.
+REM if we change the images, rebuild this icon and re-add it to the MSVC project, I expect.
+REM NB Windows caches these so look up how to get the new icons to show up.
+
+REM requires image magick
+
+magick logo-16.png logo-24.png logo-32.png logo-48.png logo-64.png logo-96.png logo-128.png logo-256.png logo-512.png piano_fest_icons_win.ico
+  
