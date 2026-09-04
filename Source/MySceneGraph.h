170e909de443acd57b6d5710165ad8ac9a8e2278
Juliet <juliet@amju.com>
2026-09-04 10:19:26 +0100

    Just a note about lifetime

--- Source/MySceneGraph.h
+++ Source/MySceneGraph.h
@@ -8,6 +8,7 @@ namespace Amju
 // This scene graph instance lives from the first call to
 //  GetScenGraph() until it is destroyed by ResetSceneGraph()
 //  or at the end of the process.
+// (Don't leave it too late, we don't want a crash on exit :( )
 
 // * GetSceneGraph *
 // For all 3D scenes, this is the scene graph.
