b941f5ab02fe2ecec552730fb6754dc75a636cb0
Juliet <juliet@amju.com>
2026-09-14 18:09:25 +0100

    BugSplat integration: ignore secret creds file; add template for new repo clone.

---
+++ Script/Msvc/BugSplat/env.template.ps1
@@ -0,0 +1,7 @@
+# New machine? Clean repo?
+# Copy this file to env.ps1 and fill in your unique BugSplat keys.
+# To generate keys, go to: https://app.bugsplat.com/v2/database/integrations?database=amju-games#oauth
+
+# DO NOT commit the populated env.ps1 file to Git!
+$env:SYMBOL_UPLOAD_CLIENT_ID="PASTE_YOUR_CLIENT_ID_HERE"
+$env:SYMBOL_UPLOAD_CLIENT_SECRET="PASTE_YOUR_CLIENT_SECRET_HERE"
