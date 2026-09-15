0f71953c3a2784b27daad629995781f475382621
Juliet <juliet@amju.com>
2026-09-15 14:48:01 +0100

    Fix comment

--- Source/AutoRepeatFilter.h
+++ Source/AutoRepeatFilter.h
@@ -7,7 +7,7 @@ struct KeyEvent;
 // Filter auto-repeat key events, where additional key down 
 //  events are fired with no intervening key up event.
 // Returns true if event is an auto-repeat event.
-// TODO Currently ignores modifiers and special keys.
+// TODO Currently ignores modifiers.
 bool IsAutoRepeat(const KeyEvent& ke);
 }
 
